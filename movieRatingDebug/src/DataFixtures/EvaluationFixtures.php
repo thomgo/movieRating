746bbb316068b81581b705fafb636f6849343469
Thomas <tg.59290@gmail.com>
2020-03-05 15:34:26 +0100

    copy functional app

---
+++ movieRatingDebug/src/DataFixtures/EvaluationFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\DataFixtures\UserFixtures;
+use App\DataFixtures\MovieFixtures;
+use App\Entity\Evaluation;
+use App\Entity\User;
+use App\Entity\Movie;
+use Faker;
+
+class EvaluationFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+      $faker = Faker\Factory::create('fr_FR');
+
+      for ($i=0; $i < 40; $i++) {
+        $evaluation = new Evaluation();
+        $evaluation->setComment($faker->text($maxNbChars = 150));
+        $evaluation->setGrade(random_int(0,10));
+        $random = random_int(0,4);
+        $evaluation->setUser($this->getReference("user$random"));
+        $random = random_int(0,9);
+        $evaluation->setMovie($this->getReference("movie$random"));
+        $manager->persist($evaluation);
+      }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            UserFixtures::class,
+            MovieFixtures::class,
+        );
+    }
+  }
---
+++ movieRatingDebug/src/Form/EvaluationType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evaluation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class EvaluationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('comment', null, ['label' => 'Commentaire'])
+            ->add('grade', null, ['label' => 'Note', "attr" => [
+                "min" => "0",
+                "max" => "10"
+              ]])
+            ->add('Enregistrer', SubmitType::class, ['attr'=>[
+                "class" => "secondBg d-block mx-auto",
+              ]])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Evaluation::class,
+        ]);
+    }
+}
