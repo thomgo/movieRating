5ea12772dee6d01b5325d9a5577d43381c19dd50
Thomas <tg.59290@gmail.com>
2020-03-06 14:41:38 +0100

    add test for getaverage method inn Movie entity

---
+++ movieRating/tests/Entity/MovieTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Tests\Entity;
+
+use App\Entity\Movie;
+use App\Entity\Evaluation;
+use PHPUnit\Framework\TestCase;
+
+
+class MovieTest extends TestCase
+{
+
+/**
+ * @dataProvider provideGradesData
+ */
+public function testGetAverage($grades, $expectedResult)
+{
+  $movie = new Movie();
+  foreach ($grades as $grade) {
+    var_dump($grade);
+    $evaluation = new Evaluation();
+    $evaluation->setGrade($grade);
+    // var_dump($evaluation->getGrade());
+    $movie->addEvaluation($evaluation);
+  }
+    $this->assertEquals($expectedResult, $movie->getAverage());
+}
+
+public function provideGradesData()
+{
+  return [
+    [[0,0,0,0,0], 0],
+    [[0], 0],
+    [[5,2,4,0,0,10], 3.5],
+    [[2,2,2,2,2], 2],
+    [[1,9,4,4,7], 5],
+    [[7,9,5,3,5,1,7], 5.29],
+  ];
+}
+
+}
